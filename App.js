5f8da5b09723166a5eae3ceb798756d50629ffb6
Sunil Kumar <sunilpalarya@gmail.com>
2024-03-07 11:40:52 +0530

    feature: UI changes

--- App.js
+++ App.js
@@ -1,28 +1,31 @@
-import { createAppContainer } from 'react-navigation';
-import IndexScreen from './src/screens/IndexScreen';
-import { createStackNavigator } from 'react-navigation-stack';
-import { Provider } from './src/context/BlogContext';
-import ShowScreen from './src/screens/ShowScreen';
-import CreateScreen from './src/screens/CreateScreen';
-import EditScreen from './src/screens/EditScreen';
+import { createAppContainer } from "react-navigation";
+import IndexScreen from "./src/screens/IndexScreen";
+import { createStackNavigator } from "react-navigation-stack";
+import { Provider } from "./src/context/BlogContext";
+import ShowScreen from "./src/screens/ShowScreen";
+import CreateScreen from "./src/screens/CreateScreen";
+import EditScreen from "./src/screens/EditScreen";
 
 const navigator = createStackNavigator(
   {
     Index: IndexScreen,
     Show: ShowScreen,
     Create: CreateScreen,
-    Edit: EditScreen
+    Edit: EditScreen,
   },
   {
-    initialRouteName: 'Index',
+    initialRouteName: "Index",
     defaultNavigationOptions: {
-      title: 'Blogs'
-    }
-});
+      title: "Blogs",
+    },
+  }
+);
 
 const App = createAppContainer(navigator);
 export default () => {
-  return (<Provider>
+  return (
+    <Provider>
       <App />
-  </Provider>)
-}
+    </Provider>
+  );
+};
